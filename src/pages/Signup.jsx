09eadc83e8f7fe2d793b9cd99f4a6030576c86e1
JB <drv98@naver.com>
2023-12-06 11:18:13 +0900

    가입하기

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,26 +1,31 @@
-import { useState } from 'react'
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { useState } from 'react';
+import { auth } from '../firebase/config';
 
 export default function Signup() {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    console.log(email, password)
-  }
-  
+    e.preventDefault();
+    setError(null);
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((res) => {
+        console.log('유저 가입: ', res.user);
+      })
+      .catch((err) => {
+        setError(err.message);
+      });
+  };
+
   return (
     <div>
-      <h2>Signup</h2>
+      <h2>회원가입</h2>
       <form onSubmit={handleSubmit}>
         <label>
           <span>email:</span>
-          <input
-            required
-            type="email"
-            onChange={(e) => setEmail(e.target.value)}
-            value={email}
-          />
+          <input required type="email" onChange={(e) => setEmail(e.target.value)} value={email} />
         </label>
         <label>
           <span>password:</span>
@@ -32,7 +37,8 @@ export default function Signup() {
           />
         </label>
         <button>sign up</button>
+        {error && <p>{error}</p>}
       </form>
     </div>
-  )
+  );
 }
