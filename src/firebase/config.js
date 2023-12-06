7305eb5479c434beefa2302e485436fd3dc67b5d
JB <drv98@naver.com>
2023-12-06 10:05:52 +0900

    파이어베이스 설치및 설정

---
+++ src/firebase/config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBClqO5fBWOpca9bnTebzgvzeRTuEMr0z4',
+  authDomain: 'todolist-drv98.firebaseapp.com',
+  projectId: 'todolist-drv98',
+  storageBucket: 'todolist-drv98.appspot.com',
+  messagingSenderId: '496208153776',
+  appId: '1:496208153776:web:2ab44b390e59c18f0dff75',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
