311d3bafe3ef9f14b69fdc9d5a66239c346598ab
JB <drv98@naver.com>
2023-12-06 10:34:24 +0900

    할일 추가기능

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,11 +1,16 @@
+import { addDoc, collection } from 'firebase/firestore';
 import { useState } from 'react';
+import { db } from '../firebase/config';
 
 export default function TodoForm() {
   const [newTodo, setNewTodo] = useState('');
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(newTodo);
+    const ref = collection(db, 'todos');
+    await addDoc(ref, {
+      title: newTodo,
+    });
 
     setNewTodo('');
   };
@@ -14,12 +19,7 @@ export default function TodoForm() {
     <form onSubmit={handleSubmit}>
       <label>
         <span>새 할일 :</span>
-        <input
-          required
-          type="text"
-          onChange={(e) => setNewTodo(e.target.value)}
-          value={newTodo}
-        />
+        <input required type="text" onChange={(e) => setNewTodo(e.target.value)} value={newTodo} />
       </label>
       <button>추가</button>
     </form>
