dec3ba75848fd4b3e599e8fed2e0c9f1dbf8d6f4
JB <drv98@naver.com>
2023-12-06 11:24:01 +0900

    로그아웃

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,18 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { auth } from '../firebase/config';
 
 export default function Navbar() {
+  const logout = () => {
+    signOut(auth)
+      .then(() => {
+        console.log('유저 로그아웃');
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
   return (
     <nav>
       <h1>My Todo List</h1>
@@ -15,7 +26,7 @@ export default function Navbar() {
         <li>
           <Link to="/signup">가입</Link>
         </li>
-        <li>로그아웃</li>
+        <li onClick={logout}>로그아웃</li>
       </ul>
     </nav>
   );
